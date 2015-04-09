38b88437891a2ee0292c6d534609653b5ccaa901
hydream <hydream@gmail.com>
2015-04-10 02:27:18 +0800

    fix

---
+++ addIPtoHosts.sh
@@ -0,0 +1,3 @@
+#vi ansible hosts
+echo "!!!!!!!!!!!!!add ip of three container"
+vi /etc/ansible/TomcatPlaybook/hosts
---
+++ runPlaybookAndPublish.sh
@@ -0,0 +1,11 @@
+#run Playbook
+cd /etc/ansible/TomcatPlaybook
+ansible-playbook -i hosts site.yml
+
+#------------------------------------------------------------------------------------------------------------------------------
+#Github git test file or get file from local CI, use ansible publish to muti servers
+echo "Github git test file or get file from local CI, use ansible publish to muti servers"
+cd /tmp
+git clone https://github.com/hydream/test
+cd /etc/ansible/TomcatPlaybook
+ansible  -i hosts -m copy -a 'src=/tmp/test dest=/usr/share/tomcat/webapps force=yes'
